de63ede1ea98fc68e6558903ef149e9d81470658
Elliott Lockwood <lockwood721@gmail.com>
2020-07-07 19:23:48 -0500

    Repo created and array completed

---
+++ index.js
@@ -0,0 +1,76 @@
+var inquirer = require("inquirer");
+
+
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is your project name?",
+        name: "Title"
+      },
+      {
+        type: "input",
+        message: "Please describe your project.",
+        name: "Description" 
+      },
+      {
+        type: "input",
+        message: "Please necessary steps for installation.",
+        name: "Installation"
+      },
+      {
+        type: "input",
+        message: "How will this application be used?",
+        name: "Usage"
+      },
+      {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "License"
+      },
+      {
+        type: "input",
+        message: "What do I need to know to contribute to this repo?",
+        name: "Contributing"
+      },
+      {
+        type: "input",
+        message: "What command do I need to make to run tests?",
+        name: "Tests"
+      },
+      {
+        type: "input",
+        message: "If you have questions this is how you can contact me.",
+        name: "Questions"
+      },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    inquirer
+  .prompt(questions)
+  .then(function(response) {
+
+    if (response.confirm === response.password) {
+      console.log("Success!");
+    }
+    else {
+      console.log("You forgot your password already?!");
+    }
+  });
+}
+
+// function call to initialize program
+init();
+
+
+
+
+    
+
+  
